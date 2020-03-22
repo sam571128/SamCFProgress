60d894f40598acedbacbf9a239d5709fe0a99e94
sam571128 <samlee.571128@gmail.com>
2020-03-22 14:11:00 +0800

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/73935202

---
+++ codeforces/4/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    long long int a;
+    cin >> a;
+	
+	cout << ((a%2==0&&a!=2)?"YES":"NO");
+    return 0;
+}
