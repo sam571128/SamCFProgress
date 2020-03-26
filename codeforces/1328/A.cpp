bdea00c2d28f7fa40d8229e5a132fe461dcfb565
sam571128 <samlee.571128@gmail.com>
2020-03-26 22:39:00 +0800

    Add solution for problem `A - Divisibility Problem` Link: https://codeforces.com/contest/1328/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1328/submission/74403826

--- codeforces/1328/A.cpp
+++ codeforces/1328/A.cpp
@@ -1,27 +1,15 @@
 #include <bits/stdc++.h>
-#include <cmath>
 
-#define ll long long 
 #define fastio ios_base::sync_with_stdio(0);cin.tie(0);
-#define push emplace_back
-#define pop pop_front
-#define cini(i) int i;cin>>i;
-#define cinll(l) int l;cin >> l;
-#define intz(z) int z = 0;
 using namespace std;
-typedef vector<int> vi; 
 
 int main(){
 	fastio
-	int n;
-	cin >> n;
-	while(n--){
+	int t;
+	cin >> t;
+	while(t--){
 		int a,b;
 		cin >> a >> b;
-		if(a%b!=0){
-			cout << b*(a/b+1)-a << "\n";
-		}else{
-			cout << 0 << "\n";
-		}
-	}
+		cout << (a%b==0 ? 0 : b-(a%b))<< "\n";
+	} 
 }
