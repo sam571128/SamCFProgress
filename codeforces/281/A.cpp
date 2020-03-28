6e1b7b1d809f1c9f096a82cb62c75ae65435aca6
sam571128 <samlee.571128@gmail.com>
2020-03-28 11:06:00 +0800

    Add solution for problem `A - Word Capitalization` Link: https://codeforces.com/contest/281/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/281/submission/74600322

---
+++ codeforces/281/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+int main(){
+	fastio
+	string a;
+	cin >> a;
+	a[0] = toupper(a[0]);
+	cout << a;
+}
