8fe50a9d22ebe42f00635775a410f1a09bea5e10
sam571128 <samlee.571128@gmail.com>
2020-03-28 12:35:00 +0800

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/74603886

---
+++ codeforces/617/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+
+int main(){
+	fastio
+	int a;
+	cin >> a;
+	cout << ceil(a/5.0);
+}
