aa7f757eb7674c2b465fb438be2dc26323564abc
sam571128 <samlee.571128@gmail.com>
2020-04-14 08:16:00 +0800

    Add solution for problem `A - Candies and Two Sisters` Link: https://codeforces.com/contest/1335/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1335/submission/76645902

---
+++ codeforces/1335/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
+#define push push_back
+#define pop pop_front
+#define cini(i) int i;cin>>i;
+#define cinll(l) int l;cin >> l;
+#define cins(s) string s;cin >> s;
+#define intz(z) int z = 0;
+using namespace std;
+typedef vector<int> vi;
+typedef vector<ll int> vll;
+typedef vector<string> vs;
+
+/********SAM571128's SOLUTION**********/
+int main(){
+	fastio
+	int t;
+	cin >> t;
+	while(t--){
+		int a;
+		cin >> a;
+		cout << a/2 - (a%2 ? 0 : 1) << "\n";
+	}
+}
