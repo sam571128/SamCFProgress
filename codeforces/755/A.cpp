b4b314ce508a372c286489750bad58babcf57cfb
sam571128 <samlee.571128@gmail.com>
2020-04-18 13:25:00 +0800

    Add solution for problem `A - PolandBall and Hypothesis` Link: https://codeforces.com/contest/755/problem/A Tags: brute force, graphs, math, number theory, *800 Ref: https://codeforces.com/contest/755/submission/77143961

---
+++ codeforces/755/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define pf pop_front
+#define cini(i) int i;cin>>i;
+#define cinll(l) int l;cin >> l;
+#define cins(s) string s;cin >> s;
+#define intz(z) int z = 0;
+using namespace std;
+typedef vector<int> vi;
+typedef vector<ll int> vll;
+typedef vector<string> vs;
+
+
+int main(){
+	fastio
+	int n;
+	cin >> n;
+	if(n==1)
+		cout << 3;
+	else if(n==2)
+		cout << 4;
+	else
+		cout << n-2;
+}
