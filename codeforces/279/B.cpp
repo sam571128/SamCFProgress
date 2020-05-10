a816f102ea64cd83d463c06710052a95b339f183
sam571128 <samlee.571128@gmail.com>
2020-05-10 20:48:00 +0800

    Add solution for problem `B - Books` Link: https://codeforces.com/contest/279/problem/B Tags: binary search, brute force, implementation, two pointers, *1400 Ref: https://codeforces.com/contest/279/submission/79653610

--- codeforces/279/B.cpp
+++ codeforces/279/B.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 
 #define ll long long
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
+typedef vector<int> vi;
+typedef vector<ll int> vll;
+typedef vector<string> vs;
+typedef vector< pair<int,int> > vpi;
 
 int main(){
 	fastio
 	int n,t;
 	cin >> n >> t;
-	int arr[n];
-	for(int i = 0;i < n;i++)
-		cin >> arr[i];
-	int l = 0,r = 0, ans = 0, time = 0;
-	while(l < n){
-		while(r < n&&time+arr[r]<=t){
-			time+=arr[r];
-			r++;
-		}
-		ans = max(ans,r-l);
-		time-=arr[l];
-		l++;
+	int pref[n];
+	cin >> pref[0];
+	for(int i = 1;i < n;i++){
+		cin >> pref[i];
+		pref[i] += pref[i-1];
 	}
-	cout << ans << "\n";
-	cerr << "Time : " << (double) clock() / (double) CLOCKS_PER_SEC << "s\n";
+	int ans = 0;
+	for(int l = 0;l < n;l++){
+		int low = l;
+		int high = n-1;
+		int mid = (low+high)/2;
+		while(low<=high){
+			mid = (low+high)/2;
+			if(pref[mid]-(l==0 ? 0 : pref[l-1])==t){
+				ans = max(ans,mid-l+1);
+				break;
+			}else if(pref[mid]-(l==0 ? 0 : pref[l-1])<t){
+				low = mid+1;
+			}else{
+				high = mid-1;
+			}
+		}
+		ans = max(ans,min(low,high)-l+1);
+	}
+	cout << ans;
 }
