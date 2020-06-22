cf159e6aecfbcb30501507f77af77b983b62146b
sam571128 <samlee.571128@gmail.com>
2020-06-23 07:19:00 +0800

    Add solution for problem `A - Multiplication 1` Link: https://atcoder.jp/contests/abc169/tasks/abc169_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc169/submissions/14625462

---
+++ atcoder/abc169/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int main(){
+	fastio
+	int a,b;
+	cin >> a >> b;
+	cout << a*b << "\n";
+}
