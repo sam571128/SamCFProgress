bf1239cbd8c898919cc989cf026d70f1925db09f
sam571128 <samlee.571128@gmail.com>
2020-06-24 09:18:00 +0800

    Add solution for problem `C - Circle of Monsters` Link: https://codeforces.com/contest/1334/problem/C Tags: brute force, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/1334/submission/84844977

--- codeforces/1334/C.cpp
+++ codeforces/1334/C.cpp
@@ -8,15 +8,20 @@ using namespace std;
 void solve(){
 	ll n;
 	cin >> n;
-	array<ll,2> arr[n];
-	for(ll i = 0;i < n;i++) cin >> arr[i][0] >> arr[i][1];
-	ll ans = 0, mi = 1e18;
-	for(ll i = 0;i < n;i++){
-		mi = min({arr[i][1],arr[(i+1)%n][0], mi});
-		arr[(i+1)%n][0] = max((ll)0,arr[(i+1)%n][0]-arr[i][1]);
-		ans += arr[(i+1)%n][0];
+	ll ans = 0;
+	ll mi = 1e18;
+	vector<array<ll,2>> v(n);
+	for(ll i = 0, a, b;i < n;i++){
+		cin >> v[i][0] >> v[i][1];
 	}
-	cout << ans + mi << "\n";
+	for(ll i = 0;i < n;i++){
+		ll ni = (i+1)%n;
+		ll val = min(v[ni][0],v[i][1]);
+		ans += v[ni][0] - val;
+		mi = min(val, mi);
+	}
+	ans += mi;
+	cout << ans << "\n";
 }
 
 int main(){
