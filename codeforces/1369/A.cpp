f96b2986deba1f4db06bbbf0b7b37fdc482d9e02
sam571128 <samlee.571128@gmail.com>
2020-06-25 15:53:00 +0800

    Add solution for problem `A - FashionabLee` Link: https://codeforces.com/contest/1369/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1369/submission/84949535

---
+++ codeforces/1369/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int main(){
+	fastio
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		cout << ((n%4)!=0 ? "NO\n" : "YES\n");
+	}
+}
