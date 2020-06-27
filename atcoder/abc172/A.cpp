6964c07960b930331e618b1d80fb71d8bbb992b4
sam571128 <samlee.571128@gmail.com>
2020-06-27 20:00:00 +0800

    Add solution for problem `A - Calc` Link: https://atcoder.jp/contests/abc172/tasks/abc172_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc172/submissions/14721055

---
+++ atcoder/abc172/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int main(){
+	fastio
+	int a;
+	cin >> a;
+	cout << a+a*a+a*a*a;
+}
