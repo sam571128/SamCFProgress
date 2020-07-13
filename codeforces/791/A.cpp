6d940a2a5141408a8b8f3bbff030ba97a30e9d76
sam571128 <samlee.571128@gmail.com>
2020-07-13 16:36:00 +0800

    Add solution for problem `A - Bear and Big Brother` Link: https://codeforces.com/contest/791/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/791/submission/86731421

---
+++ codeforces/791/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long 
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+ 
+int main(){
+	fastio
+	int a,b,c;
+	cin >> a >> b;
+	int count = 0;
+	while(a<=b){
+		count++;
+		a*=3;
+		b*=2;
+	}
+	cout << count;
+}
