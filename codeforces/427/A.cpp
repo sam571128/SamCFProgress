b182045c399c517a19a137db490c7aeb391f2e02
sam571128 <samlee.571128@gmail.com>
2020-07-13 14:47:00 +0800

    Add solution for problem `A - Police Recruits` Link: https://codeforces.com/contest/427/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/427/submission/86724983

---
+++ codeforces/427/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long 
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);
+#define push push_back
+#define cini(i) int i;cin>>i;
+#define cinll(l) int l;cin >> l;
+#define intz(z) int z = 0;
+using namespace std;
+typedef vector<int> vi; 
+ 
+int main(){
+	fastio
+	cini(t);
+	intz(sum);
+	intz(count);
+	while(t--){
+		cini(tmp);
+		if(sum+tmp<0){
+			count++;
+		}else{
+			sum+=tmp;
+		}
+	} 
+	cout << count;
+}
