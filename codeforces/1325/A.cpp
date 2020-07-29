d2e175842bd7b3ee29de081c996a4b321452a377
sam571128 <samlee.571128@gmail.com>
2020-07-30 07:34:00 +0800

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/88390313

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+signed main(){
+	fastio
+	int t;
+	cin >> t;
+	while(t--){
+		int x;
+		cin >> x;
+		cout << 1 << " " << x-1 << "\n";
+	}
+}
