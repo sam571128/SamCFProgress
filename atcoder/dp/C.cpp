b034693d4327c522cff56c74da3e66b0dc9103fe
sam571128 <samlee.571128@gmail.com>
2020-08-03 23:45:00 +0800

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/15672405

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -9,15 +9,14 @@ signed main(){
 	fastio
 	int n;
 	cin >> n;
-	int a[n][3];
-	for(int i = 0;i < n;i++) cin >> a[i][0] >> a[i][1] >> a[i][2];
+	int a[n],b[n],c[n];
+	for(int i = 0;i < n;i++) cin >> a[i] >> b[i] >> c[i];
 	int dp[n][3];
-	memset(dp,0,sizeof dp);
-	dp[0][0] = a[0][0], dp[0][1] = a[0][1], dp[0][2] = a[0][2];
+	dp[0][0] = a[0], dp[0][1] = b[0], dp[0][2] = c[0];
 	for(int i = 1;i < n;i++){
-		dp[i][0] = max(dp[i-1][1]+a[i][0], dp[i-1][2] + a[i][0]);
-		dp[i][1] = max(dp[i-1][0]+a[i][1], dp[i-1][2] + a[i][1]);
-		dp[i][2] = max(dp[i-1][0]+a[i][2], dp[i-1][1] + a[i][2]);
+		dp[i][0] = max(dp[i-1][1],dp[i-1][2]) + a[i];
+		dp[i][1] = max(dp[i-1][0],dp[i-1][2]) + b[i];
+		dp[i][2] = max(dp[i-1][1],dp[i-1][0]) + c[i];
 	}
-	cout << max({dp[n-1][0],dp[n-1][1], dp[n-1][2]}) << "\n";
+	cout << max({dp[n-1][0],dp[n-1][1],dp[n-1][2]}) << "\n";
 }
