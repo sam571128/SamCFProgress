1f904912013845bceacefc433df26081baaaaff6
sam571128 <samlee.571128@gmail.com>
2020-08-04 00:35:00 +0800

    Add solution for problem `N - Slimes` Link: https://atcoder.jp/contests/dp/tasks/dp_n Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/15673169

--- atcoder/dp/N.cpp
+++ atcoder/dp/N.cpp
@@ -9,24 +9,21 @@ signed main(){
 	fastio
 	int n;
 	cin >> n;
-	int a[n];
-	vector<int> pref(n+1,0);
+	int a[n], b[n];
+	for(auto &x : a) cin >> x;
 	for(int i = 0;i < n;i++){
-		cin >> a[i];
-		pref[i+1] += a[i] + pref[i];
+		b[i] = a[i];
+		if(i!=0) b[i] += b[i-1];
 	}
 	int dp[n][n];
 	memset(dp,0,sizeof dp);
-	for(int i = n-1;i >= 0;i--){
-		for(int j = i+1;j < n;j++){
-			if(i==j) dp[i][j] = a[i];
-			else{
-				dp[i][j] = 1e18;
-				for(int k = i;k <= j;k++){
-					dp[i][j] = min(dp[i][j],dp[i][k] + dp[k+1][j] + pref[j+1]-pref[i]);
-				}
+	for(int i = 1;i < n;i++)
+		for(int j = 0;j+i < n;j++){
+			int l = j, r = j+i;
+			dp[l][r] = 1e18;
+			for(int k = l;k < r;k++){
+				dp[l][r] = min(dp[l][r],dp[l][k]+dp[k+1][r]+ b[r]-(l==0 ? 0 : b[l-1]));
 			}
 		}
-	}
 	cout << dp[0][n-1] << "\n";
 }
