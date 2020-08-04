3ac7de20d393a6f8c7bc84d1742a30be42e579a8
sam571128 <samlee.571128@gmail.com>
2020-08-04 09:51:00 +0800

    Add solution for problem `Q - Flowers` Link: https://atcoder.jp/contests/dp/tasks/dp_q Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/15677305

--- atcoder/dp/Q.cpp
+++ atcoder/dp/Q.cpp
@@ -5,46 +5,35 @@
 
 using namespace std;
 
+
 const int N = 2e5+5;
 
-struct SegT{
-	int node[4*N];
+int h[N], a[N], BIT[N];
+int n;
 
-	void modify(int pos, int val, int ind = 1, int l = 0, int r = N-1){
-		if(l==r){
-			node[ind] = val;
-		}else{
-			int mid = (l+r)/2;
-			if(pos<=mid) modify(pos,val,ind<<1,l,mid);
-			else modify(pos,val,ind<<1|1,mid+1,r);
-			node[ind] = max(node[ind<<1],node[ind<<1|1]);
-		}
+void update(int i, int v){
+	while(i < N){
+		BIT[i] = max(BIT[i],v);
+		i += i&-i;
 	}
+}
 
-	int range_query(int ql, int qr, int ind = 1, int l = 0, int r = N-1){
-		if(ql>r||qr<l) return INT_MIN;
-		if(ql<=l&&qr>=r) return node[ind];
-		int mid = (l+r)/2;
-		return max(range_query(ql,qr,ind<<1,l,mid),range_query(ql,qr,ind<<1|1,mid+1,r));
+int query(int i){
+	int ans = 0;
+	while(i){
+		ans = max(ans,BIT[i]);
+		i -= i&-i;
 	}
-} st;
-
-void solve(){
-	int n;
-	cin >> n;
-	int h[n], a[n];
-	for(auto &x : h) cin >> x;
-	for(auto &x : a) cin >> x;
-	for(int i = 0;i < n;i++){
-		//cout << i << " " << h[i] << " " << st.range_query(0,h[i]) << " " << a[i] << "\n";
-		st.modify(h[i],st.range_query(0,h[i]) + a[i]);
-	}
-	cout << st.range_query(0,N-1) << "\n"; 
+	return ans;
 }
 
 signed main(){
 	fastio
-	int t = 1;
-	//cin >> t;
-	while(t--) solve();
+	cin >> n;
+	for(int i = 0;i < n;i++) cin >> h[i];
+	for(int i = 0;i < n;i++) cin >> a[i];
+	for(int i = 0;i < n;i++){
+		update(h[i],query(h[i])+a[i]);
+	}
+	cout << query(n) << "\n";
 }
