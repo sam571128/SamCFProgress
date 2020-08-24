2ee045f0c65777da53ab482d8c83349ce4155697
sam571128 <samlee.571128@gmail.com>
2020-08-24 21:53:00 +0800

    Add solution for problem `B - Frog 2` Link: https://atcoder.jp/contests/dp/tasks/dp_b Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16206582

--- atcoder/dp/B.cpp
+++ atcoder/dp/B.cpp
@@ -5,25 +5,19 @@
 
 using namespace std;
 
-void solve(){
-	int n, k;
+signed main(){
+	fastio
+	int n,k;
 	cin >> n >> k;
 	int h[n];
 	for(auto &x : h) cin >> x;
-	int dp[n]; //dp[i] = The minimum cost when you stop at i-th stone
-	dp[0] = 0; //Standing on the First stone do not cost anything
-	for(int i = 1;i < n;i++){
-		dp[i] = 1e18;
-		for(int j = 1;j <= k;j++){
-			if(i-j>=0) dp[i] = min(dp[i], dp[i-j] + abs(h[i]-h[i-j]));
+	int dp[n];
+	memset(dp,0x3f3f3f,sizeof dp);
+	dp[0] = 0;
+	for(int i = 0;i < n;i++){
+		for(int j = max((int)0,i-k);j < i;j++){
+			dp[i] = min(dp[i],dp[j] + abs(h[i]-h[j]));
 		}
 	}
 	cout << dp[n-1] << "\n";
-}
-
-signed main(){
-	fastio
-	int t = 1;
-	//cin >> t;
-	while(t--) solve();
 }
