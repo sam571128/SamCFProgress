37a39057bfa7f32e2eae530a61fa34f7ea015a1d
sam571128 <samlee.571128@gmail.com>
2020-08-24 22:21:00 +0800

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16207315

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -5,29 +5,23 @@
 
 using namespace std;
 
-void solve(){
+signed main(){
+	fastio
 	int n,cp;
 	cin >> n >> cp;
 	int w[n], v[n];
 	for(int i = 0;i < n;i++) cin >> w[i] >> v[i];
-	int dp[n][cp+1]; //The maximum value for the first n items and capacity
-	for(int i = 0;i <= cp;i++) dp[0][i] = (i < w[0] ? 0: v[0]);
+	int dp[n][cp+1];
+	memset(dp,0,sizeof dp);
+	for(int i = w[0]; i <= cp; i++) dp[0][i] = v[0];
 	for(int i = 1;i < n;i++){
-		for(int	j = 0;j <= cp;j++){
-			if(j==0) dp[i][j] = 0;
-			else{
-				dp[i][j] = dp[i][j-1];
-				dp[i][j] = max(dp[i-1][j],dp[i][j]);
-				if(j >= w[i]) dp[i][j] = max(dp[i][j], dp[i-1][j-w[i]] + v[i]);
-			}
+		for(int j = 1;j <= cp;j++){
+			dp[i][j] = max({dp[i][j],dp[i][j-1]});
+			dp[i][j] = max(dp[i][j],dp[i-1][j]);
+			if(j >= w[i]) dp[i][j] = max(dp[i][j],dp[i-1][j-w[i]] + v[i]);
 		}
 	}
-	cout << dp[n-1][cp] << "\n";
-}
-
-signed main(){
-	fastio
-	int t = 1;
-	//cin >> t;
-	while(t--) solve();
+	int ans = 0;
+	for(int i = 0;i <= cp;i++) ans = max(ans,dp[n-1][i]);
+	cout << ans << "\n";
 }
