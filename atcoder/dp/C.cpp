45a7f2345cee60f310d6d45b8a371c0a7d44de1a
sam571128 <samlee.571128@gmail.com>
2020-08-24 22:00:00 +0800

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16206744

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -5,27 +5,19 @@
 
 using namespace std;
 
-void solve(){
+signed main(){
+	fastio
 	int n;
 	cin >> n;
 	int a[n][3];
 	for(int i = 0;i < n;i++) cin >> a[i][0] >> a[i][1] >> a[i][2];
-	int dp[n][3]; //dp[i][j] = The maximum point we can get on i-th day doing the j-th activity
+	int dp[n][3];
+	memset(dp,0,sizeof dp);
 	dp[0][0] = a[0][0], dp[0][1] = a[0][1], dp[0][2] = a[0][2];
 	for(int i = 1;i < n;i++){
-		for(int j = 0;j < 3;j++){
-			dp[i][j] = 0;
-			for(int k = 0;k < 3;k++){
-				if(j!=k) dp[i][j] = max(dp[i][j], dp[i-1][k] + a[i][j]);
-			}
-		}
+		dp[i][0] = max(dp[i-1][1]+a[i][0], dp[i-1][2] + a[i][0]);
+		dp[i][1] = max(dp[i-1][0]+a[i][1], dp[i-1][2] + a[i][1]);
+		dp[i][2] = max(dp[i-1][0]+a[i][2], dp[i-1][1] + a[i][2]);
 	}
-	cout << max({dp[n-1][0],dp[n-1][1],dp[n-1][2]}) << "\n";
-}
-
-signed main(){
-	fastio
-	int t = 1;
-	//cin >> t;
-	while(t--) solve();
+	cout << max({dp[n-1][0],dp[n-1][1], dp[n-1][2]}) << "\n";
 }
