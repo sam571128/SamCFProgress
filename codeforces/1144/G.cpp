e922c0f8670c638162e46df73861405b9de46835
sam571128 <samlee.571128@gmail.com>
2020-08-26 19:30:00 +0800

    Add solution for problem `G - Two Merged Sequences` Link: https://codeforces.com/contest/1144/problem/G Tags: dp, greedy, *2400 Ref: https://codeforces.com/contest/1144/submission/91045665

---
+++ codeforces/1144/G.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+const int INF = 1e18;
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> inc, dec;
+	int arr[n];
+	vector<int> ans(n,0);
+	for(auto &x : arr) cin >> x;
+	int a = -INF, b = INF;
+	for(int i = 0;i < n;i++){
+		if(a < arr[i]&&b > arr[i]){
+			if(i==n-1 || arr[i] < arr[i+1]) a = arr[i];
+			else b = arr[i], ans[i] = 1;
+		}else if(a < arr[i]){
+			a = arr[i];
+		}else if(b > arr[i]){
+			b = arr[i];
+			ans[i] = 1;
+		}else{
+			cout << "No\n";
+			return;
+		}
+	}
+	cout << "Yes\n";
+	for(auto x : ans) cout << x << " ";
+}
+
+signed main(){
+	fastio
+	int t = 1;
+	//cin >> t;
+	while(t--) solve();
+}   
