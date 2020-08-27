afb1f05e5fbd9f11687bcea96aff3594c09eeda0
sam571128 <samlee.571128@gmail.com>
2020-08-27 15:36:00 +0800

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16258251

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -5,16 +5,24 @@
 
 using namespace std;
 
-signed main(){
-	fastio
+void solve(){
 	int n;
 	cin >> n;
-	int arr[n];
-	for(auto &x : arr) cin >> x;
-	int dp[n];
-	dp[0] = 0;
+	int h[n];
+	for(auto &x : h) cin >> x;
+	int dp[n]; //dp[i] = The minimum cost when you stop at i-th stone
+	dp[0] = 0; //Standing on the First stone do not cost anything
 	for(int i = 1;i < n;i++){
-		dp[i] = min(dp[i-1]+abs(arr[i]-arr[i-1]),(i >= 2 ? dp[i-2] + abs(arr[i]-arr[i-2]) : INT_MAX));
+		dp[i] = 1e18;
+		if(i-1 >= 0) dp[i] = min(dp[i], dp[i-1] + abs(h[i]-h[i-1]));
+		if(i-2 >= 0) dp[i] = min(dp[i], dp[i-2] + abs(h[i]-h[i-2]));
 	}
 	cout << dp[n-1] << "\n";
+}
+
+signed main(){
+	fastio
+	int t = 1;
+	//cin >> t;
+	while(t--) solve();
 }
