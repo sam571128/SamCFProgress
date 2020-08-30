f4a18da173e4b6d0151f753d0b9d30c531af3a39
sam571128 <samlee.571128@gmail.com>
2020-08-30 19:56:00 +0800

    Add solution for problem `E - K Integers` Link: https://codeforces.com/contest/1269/problem/E Tags: binary search, data structures, *2300 Ref: https://codeforces.com/contest/1269/submission/91339736

--- codeforces/1269/E.cpp
+++ codeforces/1269/E.cpp
@@ -6,49 +6,44 @@
 using namespace std;
 
 const int N = 2e5+5;
-
-struct BIT{
-	int arr[N];
-	BIT(){memset(arr,0,sizeof arr);}
-	void modify(int pos, int val){
-		while(pos < N){
-			arr[pos]+=val;
-			pos += pos&(-pos);
-		}
+int arr1[N], arr2[N];
+int n;
+void modify(int *arr,int pos, int val){
+	while(pos <= n){
+		arr[pos]+=val;
+		pos += pos&(-pos);
 	}
-	int query(int pos){
-		int res = 0;
-		while(pos){
-			res += arr[pos];
-			pos -= pos&(-pos);
-		}
-		return res;
+}
+int query(int *arr, int pos){
+	int res = 0;
+	while(pos){
+		res += arr[pos];
+		pos -= pos&(-pos);
 	}
-};
+	return res;
+}
 
 int pos[N], sum;
 
 void solve(){
-	BIT t1, t2;
-	int n;
 	cin >> n;
 	for(int i = 1, tmp;i <= n;i++){
 		cin >> tmp;
 		pos[tmp] = i;
 	}
 	for(int i = 1;i <= n;i++){
-		sum += i - 1 - t1.query(pos[i]);
-		t1.modify(pos[i],1), t2.modify(pos[i],pos[i]);
+		sum += i - 1 - query(arr1,pos[i]);
+		modify(arr1,pos[i],1), modify(arr2,pos[i],pos[i]);
 		int mid, l = 1, r = n;
 		while(l<=r){
 			mid = (l+r)/2;
-			if(t1.query(mid)*2 <= i) l = mid+1;
+			if(query(arr1,mid)*2 <= i) l = mid+1;
 			else r = mid-1;
 		}
 		int ans = 0;
-		int cnt1 = t1.query(mid), cnt2 = t2.query(mid);
+		int cnt1 = query(arr1,mid), cnt2 = query(arr2,mid);
 		ans += mid*cnt1 - cnt2 - cnt1*(cnt1-1)/2;
-		cnt1 = i - cnt1, cnt2 = t2.query(n) - cnt2;
+		cnt1 = i - cnt1, cnt2 = query(arr2,n) - cnt2;
 		ans += cnt2 - cnt1*(mid+1) - cnt1*(cnt1-1)/2;
 		cout << ans + sum << " ";
 	}
