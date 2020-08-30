74c827321353cde8ec939c94fffa89933c6e56ba
sam571128 <samlee.571128@gmail.com>
2020-08-30 19:48:00 +0800

    Add solution for problem `E - K Integers` Link: https://codeforces.com/contest/1269/problem/E Tags: binary search, data structures, *2300 Ref: https://codeforces.com/contest/1269/submission/91339198

--- codeforces/1269/E.cpp
+++ codeforces/1269/E.cpp
@@ -1,57 +1,56 @@
 #include <bits/stdc++.h>
-
 #define int long long
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 using namespace std;
-
-const int N = 2e5+5;
-int arr1[N], arr2[N];
+ 
+const int maxn=2e5+10;
+int sum1[maxn];
+int sum2[maxn];
 int n;
-void modify(int *arr,int pos, int val){
-	while(pos <= n){
-		arr[pos]+=val;
-		pos += pos&(-pos);
-	}
+int lowbit(int x){
+    return x&-x;
 }
-int query(int *arr, int pos){
-	int res = 0;
-	while(pos){
-		res += arr[pos];
-		pos -= pos&(-pos);
-	}
-	return res;
+void add(int *sum,int x,int v){
+    while(x<=n){
+        sum[x]+=v;
+        x+=lowbit(x);
+    }
 }
-
-int pos[N], sum;
-
-void solve(){
-	cin >> n;
-	for(int i = 1, tmp;i <= n;i++){
-		cin >> tmp;
-		pos[tmp] = i;
-	}
-	for(int i = 1;i <= n;i++){
-		sum += i - 1 - query(arr1,pos[i]);
-		modify(arr1,pos[i],1), modify(arr2,pos[i],pos[i]);
-		int mid, l = 1, r = n;
-		while(l<=r){
-			mid = (l+r)/2;
-			if(query(arr1,mid)*2 <= i) l = mid+1;
-			else r = mid-1;
-		}
-		int ans = 0;
-		int cnt1 = query(arr1,mid), cnt2 = query(arr2,mid);
-		ans += mid*cnt1 - cnt2 - cnt1*(cnt1-1)/2;
-		cnt1 = i - cnt1, cnt2 = query(arr2,n) - cnt2;
-		ans += cnt2 - cnt1*(mid+1) - cnt1*(cnt1-1)/2;
-		cout << ans + sum << " ";
-	}
+int query(int *sum,int x){
+    int res=0;
+    while(x>0){
+        res+=sum[x];
+        x-=lowbit(x);
+    }
+    return res;
 }
-
+int pos[maxn];
 signed main(){
-	fastio
-	int t = 1;
-	//cin >> t;
-	while(t--) solve();
+    cin >> n;
+    for(int i=1, tmp;i<=n;i++){
+        cin >> tmp;
+        pos[tmp]=i;
+    }
+    int ans1=0;
+    for(int i=1;i<=n;i++){
+        ans1+=i-1-query(sum1,pos[i]);
+        add(sum1,pos[i],1);
+        add(sum2,pos[i],pos[i]);
+        int mid,l=1,r=n;
+        while(l<=r){
+            mid=(l+r)>>1;
+            if(query(sum1,mid)*2<=i){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        int ans2=0;
+        int cnt=query(sum1,mid),sum=query(sum2,mid);
+        ans2+=mid*cnt-sum-cnt*(cnt-1)/2;
+        cnt=i-cnt,sum=query(sum2,n)-sum;
+        ans2+=sum-cnt*(mid+1)-cnt*(cnt-1)/2;
+        cout << ans1 + ans2 << " ";
+    }
+    return 0;
 }
