a83f05544c705053d3c1bdfe937ffbf0f87ce07f
sam571128 <samlee.571128@gmail.com>
2020-09-06 14:12:00 +0800

    Add solution for problem `F - New Year and Handle Change` Link: https://codeforces.com/contest/1279/problem/F Tags: binary search, dp, *2800 Ref: https://codeforces.com/contest/1279/submission/91987658

---
+++ codeforces/1279/F.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+const int N = 1e6+5, INF = 1e18;
+int arr[N];
+pair<int,int> dp[N];
+int n,k,l;
+
+int update(int val){
+	for(int i = 1;i <= n;i++) dp[i] = {INF,INF};
+	dp[0] = {0,0};
+	for(int i = 1;i <= n;i++){
+		dp[i] = min(dp[i],{dp[i-1].first+arr[i],dp[i-1].second});
+		if(i-l>=0) dp[i] = min(dp[i],{dp[i-l].first+val,dp[i-l].second+1});
+	}
+	return dp[n].second;
+}
+
+int solve(){
+	int l = 0, r = 1e18, mid;
+	while(l < r){
+		mid = l+r>>1;
+		if(update(mid)<=k) r = mid;
+		else l = mid+1;
+	}
+	update(l);
+	return dp[n].first - l * k;
+}
+
+signed main(){
+	fastio
+	cin >> n >> k >> l;
+	for(int i = 1;i <= n;i++){
+		char tmp;
+		cin >> tmp;
+		arr[i] = (tmp==tolower(tmp));
+	}
+	int ans = INT_MAX;
+	ans = min(ans,solve());
+	for(int i = 1;i <= n;i++) arr[i] ^= 1;
+	ans = min(ans,solve());
+	update(0);
+	if(dp[n].second <= k) ans = 0;
+	cout << ans << "\n";
+}
