4dde8ac75ee46b47bb893c290fc2ae467aabf554
sam571128 <samlee.571128@gmail.com>
2020-09-13 20:00:00 +0800

    Add solution for problem `A - Not` Link: https://atcoder.jp/contests/abc178/tasks/abc178_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc178/submissions/16669986

---
+++ atcoder/abc178/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+void solve(){
+	int x;
+	cin >> x;
+	cout << !x << "\n";
+}
+
+signed main(){
+	fastio
+	int t = 1;
+	//cin >> t;
+	while(t--) solve();
+}
