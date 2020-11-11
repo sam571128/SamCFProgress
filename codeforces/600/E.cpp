e713e2f202550d92b96824595879eef658ec648e
sam571128 <samlee.571128@gmail.com>
2020-11-11 13:54:00 +0800

    Add solution for problem `E - Lomsat gelral` Link: https://codeforces.com/contest/600/problem/E Tags: data structures, dfs and similar, dsu, trees, *2300 Ref: https://codeforces.com/contest/600/submission/98102469

--- codeforces/600/E.cpp
+++ codeforces/600/E.cpp
@@ -1,77 +1,57 @@
 #include <bits/stdc++.h>
 
+#define int long long
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 using namespace std;
 
 const int N = 1e5+5;
-vector<int> adj[N], vec[N];
-int color[N], cnt[N], sz[N];
-long long res[N], ans[N];
 
-void dfs_size(int u, int par){
-	sz[u] = 1;
-    for(auto v : adj[u])
-        if(v != par) dfs_size(v,u), sz[u] += sz[v];
+int t,dsu[N],color[N],sz[N],mx[N],ans[N];
+map<int,int>cnt[N];
+vector<int> adj[N];
+
+void relax(int s,int x,int u){
+	if(x>mx[s])mx[s]=x,ans[s]=u;
+	else if(x==mx[s])ans[s]+=u;
 }
-
-void update(int &mx, int u, int x){
-	ans[cnt[color[u]]] -= color[u];
-	cnt[color[u]] += x;
-	ans[cnt[color[u]]] += color[u];
-	mx = max(cnt[color[u]],mx);
+void merge(int s,int u,int v){
+	for(auto [a,b] :cnt[v])
+		relax(s,cnt[u][a]+=b,a);
 }
-
-int dfs(int u, int par, bool keep){
-	int mx = 1, heavy = 0;
-	for(auto v : adj[u]){
-		if(v != par && sz[v] > mx){
-			mx = sz[v];
-			heavy = v;
-		}
+void dfs(int u,int par){
+	sz[u]=1;
+	if(!adj[u].size() || adj[u].size()==1 && adj[u].back()==par){
+		dsu[u]=t;
+		cnt[t++][color[u]]=1;
+		ans[u]=color[u];
+		mx[u]=1;
+		return;
 	}
-	for(auto v : adj[u]){
-		//DFS 輕鏈
-		if(v != par && v != heavy){
-			//Keep = 0 代表 dfs 後會清除這個子樹的答案
-			dfs(v,u,0);
-		}
+	int heavy=N-1;
+	for(auto &v:adj[u]) if(v!=par){
+	  dfs(v,u);
+	  sz[u]+=sz[v];
+	  if(sz[heavy]<sz[v]) heavy = v;
 	}
-	mx = 0;
-	if(heavy){
-		mx = max(mx,dfs(heavy,u,1));
-		swap(vec[u],vec[heavy]);
+	dsu[u]=dsu[heavy];
+	ans[u]=ans[heavy];
+	mx[u]=mx[heavy];
+	for(auto &v:adj[u]){
+		if(v!=par && v!=heavy) merge(u,dsu[u],dsu[v]);
 	}
-	vec[u].push_back(u);
-	update(mx,u,1);
-	for(auto v : adj[u]){
-		if(v != par && v != heavy){
-			for(auto x : vec[v]){
-				update(mx,x,1);
-				vec[u].push_back(x);
-			}
-		}
-	}
-	res[u] = ans[mx];
-	if(!keep){
-		for(auto x : vec[u]){
-			update(mx,x,-1);
-		}
-	}
-	return mx;
+	relax(u,++cnt[dsu[u]][color[u]],color[u]);
 }
-
 signed main(){
 	fastio
-	int n; 
+	int n;
 	cin >> n;
-	for(int i = 1;i <= n;i++) cin >> color[i];
-	for(int i = 1,u,v;i < n;i++){
+  	for(int i = 1;i <= n;i++) cin >> color[i];
+	for(int i = 1;i < n;i++){
+		int u,v;
 		cin >> u >> v;
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
-	dfs_size(1,-1);
-	dfs(1,-1,0);
-	for(int i = 1;i <= n;i++) cout << res[i] << " ";
+	dfs(1,1);
+	for(int i = 1;i <= n;i++) cout << ans[i] << " " ;
 }
