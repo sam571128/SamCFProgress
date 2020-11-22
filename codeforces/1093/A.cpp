5ab27821228b09fcee8353bb23cc20dc6542fa9a
sam571128 <samlee.571128@gmail.com>
2020-11-22 14:47:00 +0800

    Add solution for problem `A - Dice Rolling` Link: https://codeforces.com/contest/1093/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1093/submission/99224701

---
+++ codeforces/1093/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+void solve(){
+	int n;
+	cin >> n;
+	cout << n/2 << "\n";
+}
+
+signed main(){
+	fastio
+	int t = 1;
+	cin >> t;
+	while(t--) solve();
+}
