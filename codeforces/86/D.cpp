b7f25ddb9433be1d501d21b38891702192a76fcc
sam571128 <samlee.571128@gmail.com>
2020-11-27 12:40:00 +0800

    Add solution for problem `D - Powerful array` Link: https://codeforces.com/contest/86/problem/D Tags: data structures, implementation, math, two pointers, *2200 Ref: https://codeforces.com/contest/86/submission/99685166

--- codeforces/86/D.cpp
+++ codeforces/86/D.cpp
@@ -1,6 +1,3 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
 #include <bits/stdc++.h>
 
 #define ll long long
