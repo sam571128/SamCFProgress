3e5ee9c1c72a262371d4656d210a5565e66310b2
sam571128 <samlee.571128@gmail.com>
2020-12-13 20:01:00 +0800

    Add solution for problem `A - ABC Preparation` Link: https://atcoder.jp/contests/abc185/tasks/abc185_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc185/submissions/18722639

---
+++ atcoder/abc185/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+void solve(){
+	int a,b,c,d;
+	cin >> a >> b >> c >> d;
+	cout << min({a,b,c,d}) << "\n";
+}
+
+signed main(){
+	fastio
+	int t = 1;
+	//cin >> t;
+	while(t--) solve();
+}
