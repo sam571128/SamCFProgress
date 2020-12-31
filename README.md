832461d7e6816c464221a19f7e8269a1344738aa
sam571128 <samlee.571128@gmail.com>
2020-12-31 10:05:00 +0800

    Add solution for problem `E - Apollo versus Pan` Link: https://codeforces.com/contest/1466/problem/E Tags: bitmasks, brute force, math Ref: https://codeforces.com/contest/1466/submission/102875619

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ A repository to keep track of problem solving practise, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-927 | [E - Sum Queries?](https://codeforces.com/contest/1217/problem/E) | [GNU C++17 (64)](./codeforces/1217/E.cpp) | `data structures` `greedy` `implementation` `math` `*2300` | Dec/31/2020 12:16 | 
+928 | [E - Sum Queries?](https://codeforces.com/contest/1217/problem/E) | [GNU C++17 (64)](./codeforces/1217/E.cpp) | `data structures` `greedy` `implementation` `math` `*2300` | Dec/31/2020 12:16 | 
+927 | [E - Apollo versus Pan](https://codeforces.com/contest/1466/problem/E) | [GNU C++17 (64)](./codeforces/1466/E.cpp) | `bitmasks` `brute force` `math` | Dec/31/2020 10:05 | 
 926 | [B - Alyona and a tree](https://codeforces.com/contest/739/problem/B) | [GNU C++17](./codeforces/739/B.cpp) | `binary search` `data structures` `dfs and similar` `graphs` `trees` `*1900` | Dec/26/2020 20:44 | 
 925 | [F2 - Wrong Answer on test 233 (Hard Version)](https://codeforces.com/contest/1227/problem/F2) | [GNU C++17](./codeforces/1227/F2.cpp) | `combinatorics` `math` `*2400` | Dec/26/2020 13:54 | 
 924 | [F1 - Wrong Answer on test 233 (Easy Version)](https://codeforces.com/contest/1227/problem/F1) | [GNU C++17](./codeforces/1227/F1.cpp) | `dp` `*2200` | Dec/25/2020 16:41 | 
---
+++ codeforces/1466/E.cpp
@@ -0,0 +1,65 @@
+#pragma GCC optimize("O2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,sse,sse2,fma")
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+const int MOD = 1e9+7;
+
+void solve(){
+	int n;
+	cin >> n;
+	int arr[n];
+	for(auto &x : arr) cin >> x;
+	int sum = 0;
+
+	int bit[61] = {};
+	for(auto &x : arr){
+		for(int i = 60;~i;i--){
+			if(x&(1LL<<i)) bit[i]++;
+		}
+	}
+	int tmp1[n] = {}, tmp2[n] = {};
+	for(int i = 0;i < n;i++){
+		for(int j = 60;~j;j--){
+			if(arr[i]&(1LL<<j)){
+				//cout << (1LL<<j)*bit[j]%MOD << " " << bit[j]*fastpow(2,j)%MOD << "\n";
+				tmp1[i]=(tmp1[i]+(1LL<<j)%MOD*bit[j]%MOD)%MOD;
+			}
+		}
+		/*for(int j = 0;j < n;j++){
+			tmp1[i] = (tmp1[i]+(arr[i]&arr[j]))%MOD;
+		}*/
+	}
+	for(int i = 0;i < n;i++){
+		for(int j = 60;~j;j--){
+			if(arr[i]&(1LL<<j)){
+				//cout << (1LL<<j)*bit[j]%MOD << " " << bit[j]*fastpow(2,j)%MOD << "\n";
+				tmp2[i]=(tmp2[i]+(1LL<<j)%MOD*n%MOD)%MOD;
+			}else{
+				tmp2[i]=(tmp2[i]+(1LL<<j)%MOD*bit[j]%MOD)%MOD;
+			}
+		}
+		/*for(int j = 0;j < n;j++){
+			tmp2[i] = (tmp2[i]+(arr[i]|arr[j]))%MOD;
+		}*/
+	}
+
+	for(int i = 0;i < n;i++){
+		//for(int j = 0;j < n;j++){
+			sum = (sum+tmp1[i]*tmp2[i]%MOD)%MOD;
+		//}
+	}
+	cout << sum%MOD << "\n";
+}
+
+signed main(){
+	fastio
+	int t = 1;
+	cin >> t;
+	while(t--) solve();
+}
