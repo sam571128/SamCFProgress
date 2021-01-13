e14b9a955813b15d20b0d5c76e3bff4c3dc7d11a
sam571128 <samlee.571128@gmail.com>
2021-01-14 07:40:00 +0800

    Add solution for problem `A - Heavy Rotation` Link: https://atcoder.jp/contests/abc181/tasks/abc181_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc181/submissions/19422189

---
+++ atcoder/abc181/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+signed main(){
+	fastio
+	int n;
+	cin >> n;
+	if(n&1) cout << "Black\n";
+	else cout << "White\n";
+}
